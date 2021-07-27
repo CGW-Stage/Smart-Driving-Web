8158f2b34210270503b17bc4fb350529c795ddb8
Ariol.CGW <ariol.keubeng@cgw-academy.com>
2021-07-27 21:03:58 +0200

    staging app working version

--- equipe soir/app/src/app/app.component.ts
+++ equipe soir/app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from "firebase/app"
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  afficherGoogle: boolean = true
+  menu: string = 'login'
+
+  constructor(private backend: AngularFireAuth) {
+
+  }
+
+  ngOnInit(){
+   this.backend.authState.subscribe(result => {
+     if(result){
+      this.menu = 'menu'
+     }
+     else{
+       console.log('Auhtentification échouée');
+       this.menu = 'login'
+     }
+   })
+  }
+
+  logout(){
+     this.backend.signOut()
+  }
 }
--- equipe soir/app/src/app/app.module.ts
+++ equipe soir/app/src/app/app.module.ts
@@ -12,15 +12,49 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 registerLocaleData(fr);
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule  } from "@angular/fire/auth";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzTabsModule } from 'ng-zorro-antd/tabs';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7L25Kf_snZM9f_7QvCMdVjHYE372NKYE",
+  authDomain: "smart-driving-cgw.firebaseapp.com",
+  projectId: "smart-driving-cgw",
+  storageBucket: "smart-driving-cgw.appspot.com",
+  messagingSenderId: "711835197180",
+  appId: "1:711835197180:web:3517e2d1953e20e7620179",
+  measurementId: "G-NS7EPVFEZW"
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    NzButtonModule,
+    NzIconModule,
+    NzGridModule,
+    NzTabsModule
+
   ],
   providers: [{ provide: NZ_I18N, useValue: fr_FR }],
   bootstrap: [AppComponent]
